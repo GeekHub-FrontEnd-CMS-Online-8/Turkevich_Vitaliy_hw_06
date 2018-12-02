1cdda27921183d3edb04a4b1c6d14ee730de8b32
Vitaliy Turkevich <vetalturkevich@gmail.com>
2018-12-02 15:16:23 -0500

    in progress...

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp        = require('gulp'),
+	browserSync = require('browser-sync').create(),
+    scss        = require('gulp-scss'),
+	imagemin    = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant');
+
+gulp.task("html", function () {
+    return gulp.src("src/**/*.html")
+        .pipe(gulp.dest("build"))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('scss', function () {
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(scss())
+		.pipe(gulp.dest('build/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('img', function () {
+	return gulp.src('src/img/**/*')
+        .pipe(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+		.pipe(gulp.dest('build/img'))
+		.pipe(browserSync.reload({stream: true}))
+
+});
+
+gulp.task("watch", [ 'scss', "html", 'img'], function () {
+	browserSync.init({
+		server: "build",
+		notify: false,
+		ui: {
+			port: 3000
+		}
+    });
+    gulp.watch('src/scss/**/*.scss', ["scss"]);
+    gulp.watch('src/*.html' , ["html"]);
+	gulp.watch('src/img/**/*', ["img"]);
+});
